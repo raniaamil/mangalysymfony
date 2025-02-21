f2118efef6da66f694b092e3bf66c9bdd9e96880
Rania Amil <mademoiselleamil@gmail.com>
2025-02-21 15:29:37 +0100

    Création migration

---
+++ migrations/Version20250221142248.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250221142248 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE critiques CHANGE report report TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE post CHANGE report report TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE theorie CHANGE report report TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE critiques CHANGE report report TINYINT(1) DEFAULT 0 NOT NULL');
+        $this->addSql('ALTER TABLE post CHANGE report report TINYINT(1) DEFAULT 0 NOT NULL');
+        $this->addSql('ALTER TABLE theorie CHANGE report report TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+}
